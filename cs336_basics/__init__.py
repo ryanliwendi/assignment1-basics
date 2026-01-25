c258bb290117ed13b28f3f74115cfc4e06fc7a87
ryanliwendi <liryan@usc.edu>
2026-01-25 10:45:42 -0800

    Improved professionality

--- cs336_basics/__init__.py
+++ cs336_basics/__init__.py
@@ -1,11 +1,47 @@
-import importlib.metadata
-
-__version__ = importlib.metadata.version("cs336_basics")
-
 from .bpe import train_bpe
 from .tokenizer import Tokenizer
-from .model import Linear, Embedding, RMSNorm, SwiGLU, RotaryPositionalEmbedding
-from .model import softmax, scaled_dot_product_attention
-from .model import MultiheadSelfAttention, TransformerBlock, TransformerLM
-from .train import cross_entropy, AdamW, learning_schedule, gradient_clipping
-from .train import get_batch, load_checkpoint, save_checkpoint
+
+from .model import (
+    Linear,
+    Embedding,
+    RMSNorm,
+    SwiGLU,
+    RotaryPositionalEmbedding,
+    softmax,
+    scaled_dot_product_attention,
+    MultiheadSelfAttention,
+    TransformerBlock,
+    TransformerLM,
+)
+
+from .train import (
+    cross_entropy,
+    AdamW,
+    learning_schedule,
+    gradient_clipping,
+    get_batch,
+    load_checkpoint,
+    save_checkpoint,
+)
+
+__all__ = [
+    "train_bpe",
+    "Tokenizer",
+    "Linear",
+    "Embedding",
+    "RMSNorm",
+    "SwiGLU",
+    "RotaryPositionalEmbedding",
+    "softmax",
+    "scaled_dot_product_attention",
+    "MultiheadSelfAttention",
+    "TransformerBlock",
+    "TransformerLM",
+    "cross_entropy",
+    "AdamW",
+    "learning_schedule",
+    "gradient_clipping",
+    "get_batch",
+    "load_checkpoint",
+    "save_checkpoint",
+]
